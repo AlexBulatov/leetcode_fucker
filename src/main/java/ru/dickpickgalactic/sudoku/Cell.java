0224f0723ad18f66bac105b090a8420a3766f63e
Andrew Slastenko <and.slastenko@gmail.com>
2024-01-28 17:11:20 +0700

    code style fixed

--- src/main/java/ru/dickpickgalactic/sudoku/Cell.java
+++ src/main/java/ru/dickpickgalactic/sudoku/Cell.java
@@ -4,20 +4,18 @@ import java.util.Arrays;
 import java.util.TreeSet;
 
 public class Cell {
-    public Integer value = null;
-    public TreeSet< Integer > suggested =
-            new TreeSet < Integer > (Arrays.asList (1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-    public Cell (char value)
-    {
-        if (value != '.')
-        {
-            this.value = Integer.valueOf (value - '0');
-        }
-    }
+  public Integer value = null;
+  public TreeSet<Integer> suggested =
+      new TreeSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-    public String toString ()
-    {
-        return (value == null) ? "." : value.toString ();
+  public Cell(char value) {
+    if (value != '.') {
+      this.value = value - '0';
     }
+  }
+
+  public String toString() {
+    return (value == null) ? "." : value.toString();
+  }
 }
