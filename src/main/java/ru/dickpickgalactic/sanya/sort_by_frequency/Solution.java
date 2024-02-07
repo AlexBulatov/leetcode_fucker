6d7acdd5cf8536f32a87714ddf059912069015ae
Alexander <bulka_aka_77z@gmail.com>
2024-02-07 20:17:25 +0700

    Add some extra dip

--- src/main/java/ru/dickpickgalactic/sanya/sort_by_frequency/Solution.java
+++ src/main/java/ru/dickpickgalactic/sanya/sort_by_frequency/Solution.java
@@ -7,7 +7,7 @@ import java.util.Map.Entry;
 import static ru.dickpickgalactic.util.Utils.log;
 
 /**
- * # 451. Sort Characters By Frequency
+ * <h1>451. Sort Characters By Frequency</h1>
  * <br/>
  * Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
  * <br/>
